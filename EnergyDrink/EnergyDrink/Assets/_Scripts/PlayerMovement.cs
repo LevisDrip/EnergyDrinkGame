51f4e7f4bd1470cf5d445289a47711716738c1c5
LevisDrip <127957049+LeviLamerichs@users.noreply.github.com>
2023-12-12 16:49:27 +0100

    xkfjbadkjfgaeriuykf

--- EnergyDrink/EnergyDrink/Assets/_Scripts/PlayerMovement.cs
+++ EnergyDrink/EnergyDrink/Assets/_Scripts/PlayerMovement.cs
@@ -4,24 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    float speed;
+    public float MoveSpeed;
 
+    public float JumpStrenght;
+
+    public bool IsGrounded;
+    public bool HasSlided;
+
+    public float SlideCoolDown;
+
+    private Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        SlideCoolDown = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Running();
+        SlideCoolDown -= Time.deltaTime;
+
+        Movement();
     }
 
-    public void Running()
+    public void Movement()
     {
-        transform.position += transform.right * moveSpeed * Time.deltaTime;
+        transform.position += transform.right * MoveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            IsGrounded = false;
+            //transform.position += transform.up * JumpStrenght * Time.deltaTime;
+            rigidBody.AddForce(transform.up * JumpStrenght);
+        }
+
+        if (Input.GetButtonDown("Slide") && !HasSlided)
+        {
+            Debug.Log("Slidin'");
+
+            
+            rigidBody.AddForce(transform.right * (MoveSpeed * 400));
+
+            
+
+            SlideCoolDown = 2;
+
+            
+
+            HasSlided = true;
+        }
+
+        if (SlideCoolDown <= 0)
+        {
+            HasSlided = false;
+        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touching floor");
+        IsGrounded = true;
+    }
+
+
+
 }
